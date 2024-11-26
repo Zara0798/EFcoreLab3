a8d30f7480e87c41eea97fd7e1a3661338d91497
Duaa alrefaee <duaaalrefai@gmail.com>
2024-11-26 11:47:01 +0100

    har ändrat lite på DataAccess

--- EFcore/DataAccess.cs
+++ EFcore/DataAccess.cs
@@ -14,10 +14,47 @@ namespace EFcore
 
         public DataAccess()
         {
-            _context = new TempFuktContext();
+            _context = _context;
+        }
+        /*public void LoadDataFromCsv(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines.Skip(1))  // Skip header line
+            {
+                var columns = line.Split(',');
+
+                DateTime datum = DateTime.Parse(columns[0]);
+                string plats = columns[1];
+                double temp = double.Parse(columns[2]);
+                double luftfuktighet = double.Parse(columns[3]);
+
+            }
+        }*/
+        public void LäsInCsvOchSpara(string filePath)
+        {
+            var rows = File.ReadLines(filePath)
+                           .Skip(1)  // Skippa headern
+                           .Select(line => line.Split(','))
+                           .Where(columns => columns.Length == 4)
+                           .Select(columns => new TempFuktData
+                           {
+                               Datum = DateTime.Parse(columns[0]),
+                               Plats = columns[1],
+                               Temp = double.Parse(columns[2]),
+                               Luftfuktighet = int.Parse(columns[3])
+                           }).ToList();
+
+            _context.TempFuktData.AddRange(rows);
+            _context.SaveChanges();
         }
     }
 }
-        
+
+
+
+
+
+
+
         // Metod för att fylla databasen från en CSV-fil
 
--- EFcore/TempFuktContext.cs
+++ EFcore/TempFuktContext.cs
@@ -14,6 +14,15 @@ namespace EFCore_LAB3.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = ZaraDuaa; Integrated Security = True;");
 
+       //Skapa databas om den inte redan finns
+    public void CreateDatabaseIfNotExists()
+        {
+            if (!Database.CanConnect())
+            {
+                Database.EnsureCreated();
+            }
+        }
+
         public double GetAverageTemperature(DateTime date, string location) // för att räkna medeltemp...
         {
             using TempFuktContext context = new TempFuktContext();
